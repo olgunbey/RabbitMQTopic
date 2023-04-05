d4fe90aafdb2cc72fe6fa645f4a08b875563191d
olgunbey şahin <olgunsahin0161@hotmail.com>
2023-04-05 14:15:58 +0300

    RabbitMQTopic

---
+++ RabbitMQFanoutExchange.Publish/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+factory.Port = 5672;
+
+IModel channel=factory.CreateConnection().CreateModel();
+
+
+string _exchange = "fanout-example";
+channel.ExchangeDeclare(
+    exchange:_exchange,
+    ExchangeType.Fanout
+    );
+
+
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] messagess = Encoding.UTF8.GetBytes($"olgun {i}");
+
+    channel.BasicPublish(
+        exchange:_exchange,
+        routingKey:string.Empty,
+        body:messagess
+        );
+
+}
+
+Console.Read();
---
+++ RabbitmqFanoutExchange.Consumer/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.Port = 5672;
+
+IConnection connection=factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+
+string _exchange = "fanout-example";
+channel.ExchangeDeclare(
+    exchange:_exchange,
+    ExchangeType.Fanout
+    );
+
+Console.Write("Mesaj gir: ");
+string _queuename = Console.ReadLine();
+
+channel.QueueDeclare(
+    queue: _queuename,
+    exclusive:false
+    );
+
+channel.QueueBind(
+    queue:_queuename,
+    exchange:_exchange,
+    routingKey:string.Empty
+    );
+
+EventingBasicConsumer consumers=new EventingBasicConsumer(channel);
+
+channel.BasicConsume(
+       queue: _queuename,
+       autoAck: true,
+       consumer: consumers
+       );
+
+consumers.Received += (a, b) =>
+{
+   
+
+
+    string message = Encoding.UTF8.GetString(b.Body.Span);
+
+    Console.WriteLine(message);
+};
+
+Console.Read();
+
+
+
+
